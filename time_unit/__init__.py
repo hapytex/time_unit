b06f422b8a3258dc9f9a9553d48f0ee82103503f
Willem Van Onsem <yourfriends@hapytex.eu>
2025-06-29 09:42:51 +0200

    enable extending time units

--- time_unit/__init__.py
+++ time_unit/__init__.py
@@ -1,22 +1,49 @@
+import math
 from datetime import date, datetime, timedelta
 
 
-def date_from_int(val):
+def date_from_int(val, div=1):
+    val //= div
     d = val % 100
     val //= 100
     m = val % 100
     val //= 100
     return date(val, m, d)
 
+def date_to_int(val, mul=1):
+    return mul * (val.year * 10000 + val.month * 100 + val.day)
+
 
 class TimeunitKindMeta(type):
     kind_int = None
     formatter = None
-    _registered = {}
+    _pre_registered = []
+    _registered = None
+    _multiplier = None
 
     def __init__(cls, name, bases, attrs):
         super().__init__(name, bases, attrs)
-        TimeunitKindMeta._registered[cls.kind_int] = cls
+        if cls.kind_int is not None:
+            TimeunitKindMeta._pre_registered.append(cls)
+            TimeunitKindMeta._registered = None
+            TimeunitKindMeta._multiplier = None
+
+    @property
+    def unit_register(self):
+        result = TimeunitKindMeta._registered
+        if result is None:
+            result = {k.kind_int: k for k in TimeunitKindMeta._pre_registered if k.kind_int is not None}
+            TimeunitKindMeta._registered = result
+        return result
+
+    @property
+    def multiplier(cls):
+        result = TimeunitKindMeta._multiplier
+        if result is None:
+            result = max(1, *[k.kind_int for k in TimeunitKindMeta._pre_registered])
+            result = 10 ** math.ceil(math.log10(result))
+            TimeunitKindMeta._multiplier = result
+        return result
 
     def __call__(cls, dt):
         if isinstance(dt, Timeunit):
@@ -27,13 +54,17 @@ class TimeunitKindMeta(type):
         return self.kind_int < other.kind_int
 
     def from_int(cls, val):
-        return TimeunitKindMeta._registered[val % 10](date_from_int(val // 10))
+        mul = cls.multiplier
+        return TimeunitKindMeta._registered[val % mul](date_from_int(val, mul))
 
     def get_previous(cls, dt):
         if isinstance(dt, Timeunit):
             dt = dt.dt
         dt -= timedelta(days=1)
         return cls(dt)
+
+    def first_day(cls, dt):
+        return cls.truncate(dt)
 
     def last_day(cls, dt):
         return cls._next(dt) - timedelta(days=1)
@@ -60,6 +91,18 @@ class TimeunitKind(metaclass=TimeunitKindMeta):
     kind_int = None
     formatter = None
 
+
+class Decade(TimeunitKind):
+    kind_int = 0
+    formatter = "%Ys"
+
+    @classmethod
+    def truncate(cls, dt):
+        return date(dt.year - dt.year % 10, 1, 1)
+
+    @classmethod
+    def _next(cls, dt):
+        return date(dt.year + 10, 1, 1)
 
 class Year(TimeunitKind):
     kind_int = 1
@@ -186,10 +229,10 @@ class Timeunit:
         return int(self) >= int(other)
 
     def __int__(self):
-        return self.dt.year * 100000 + self.dt.month * 1000 + self.dt.day * 10 + self.kind.kind_int
+        return date_to_int(self.dt, self.kind.multiplier) + self.kind.kind_int
 
     def __repr__(self):
-        return f"Timeunit({self.kind.__qualname__}, {self.dt!r})"
+        return f"{self.__class__.__name}({self.kind.__qualname__}, {self.dt!r})"
 
     def __str__(self):
         return self.kind.to_str(self.dt)
